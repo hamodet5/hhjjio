a9a4d222f488ee9e1730cdd917f494233e742b3f
hamodet5 <jasmmrym422@gmail.com>
2025-08-27 16:08:41 +0300

    Delete VCamera-main directory

--- VCamera-main/app/src/main/java/virtual/camera/app/bean/GmsBean.kt
+++
@@ -1,6 +0,0 @@
-package virtual.camera.app.bean
-
-data class GmsBean(val userID:Int,val userName:String,var isInstalledGms:Boolean)
-
-
-data class GmsInstallBean(val userID: Int,val success:Boolean,val msg:String)
--- VCamera-main/app/src/main/java/virtual/camera/app/settings/LogUtil.java
+++
@@ -1,9 +0,0 @@
-package virtual.camera.app.settings;
-
-import android.util.Log;
-
-public class LogUtil {
-    public static void log(String msg) {
-        Log.e("VCamera", msg);
-    }
-}
